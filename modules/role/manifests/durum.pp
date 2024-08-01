bacb4b59b0423294ecac8928c387120a00d6f0ac
Daniel Zahn <dzahn@wikimedia.org>
2024-08-01 12:53:43 -0700

    durum: include throttling class, enable it on durum2001, accept/log only
    - includes the nft throttling profile
    - includes the prometheus exporter for nft throttling data
    - Keeps it disabled on all except on durum2001
    - even on durum2001, policy is still set to "accept" so no packets are dropped
    - logging is enabled on durum2001 and disabled on other hosts
    
    Hosts: durum1001.eqiad.wmnet, durum2001.codfw.wmnet
    Change-Id: Ie5f985203af9f8b5151d871e0cadbe5ff9f2f499

--- modules/role/manifests/durum.pp
+++ modules/role/manifests/durum.pp
@@ -1,6 +1,8 @@
 class role::durum {
     include profile::base::production
     include profile::firewall
+    include profile::firewall::nftables_throttling
+    include profile::prometheus::nft_throttling_denylist
     include profile::durum
     include profile::nginx
     include profile::bird::anycast
