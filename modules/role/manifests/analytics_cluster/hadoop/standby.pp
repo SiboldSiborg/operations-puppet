364bc6c4e7db683b86b733b49bd7aae945cc49be
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-10-25 13:44:41 +0200

    Deprecate system::role for Hadoop roles
    system::role is a bit of a hack. In 21fa33b455bfd956f45525b7bb3c22c77ff3787d
    support was added to move the functionality into profile::base::production
    and simply set the role description via a Hiera flag.
    
    Change-Id: I51da314f46028d3f66b9bccce1dd0db42350e265

--- modules/role/manifests/analytics_cluster/hadoop/standby.pp
+++ modules/role/manifests/analytics_cluster/hadoop/standby.pp
@@ -2,23 +2,18 @@
 # Include standby NameNode classes
 #
 class role::analytics_cluster::hadoop::standby {
-    system::role { 'analytics_cluster::hadoop::standby':
-        description => 'Hadoop Standby NameNode',
-    }
-
-    include ::profile::java
-    include ::profile::hadoop::common
-    include ::profile::hadoop::master::standby
-    include ::profile::analytics::cluster::hadoop::yarn_capacity_scheduler
-    include ::profile::hadoop::firewall::master
+    include profile::java
+    include profile::hadoop::common
+    include profile::hadoop::master::standby
+    include profile::analytics::cluster::hadoop::yarn_capacity_scheduler
+    include profile::hadoop::firewall::master
     # This is a Hadoop client, and should
     # have any service system users it needs to
     # interacting with HDFS.
-    include ::profile::analytics::cluster::users
-    include ::profile::hadoop::backup::namenode
-    include ::profile::kerberos::client
-    include ::profile::kerberos::keytabs
-    include ::profile::firewall
-    include ::profile::base::production
-
+    include profile::analytics::cluster::users
+    include profile::hadoop::backup::namenode
+    include profile::kerberos::client
+    include profile::kerberos::keytabs
+    include profile::firewall
+    include profile::base::production
 }
--- modules/role/manifests/analytics_cluster/hadoop/yarn.pp
+++ modules/role/manifests/analytics_cluster/hadoop/yarn.pp
@@ -3,19 +3,15 @@
 # Hadoop Yarn UI
 #
 class role::analytics_cluster::hadoop::yarn {
-    system::role { 'analytics_cluster::hadoop::yarn':
-        description => 'Hadoop Yarn ResourceManager web interface'
-    }
+    include profile::java
 
-    include ::profile::java
+    include profile::tlsproxy::envoy
 
-    include ::profile::tlsproxy::envoy
+    include profile::hadoop::yarn_proxy
 
-    include ::profile::hadoop::yarn_proxy
+    include profile::kerberos::client
+    include profile::kerberos::keytabs
 
-    include ::profile::kerberos::client
-    include ::profile::kerberos::keytabs
-
-    include ::profile::firewall
-    include ::profile::base::production
+    include profile::firewall
+    include profile::base::production
 }
