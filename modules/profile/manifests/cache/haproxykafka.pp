e7a9b076f40745dc0b7039c3ceb699a5ee3d6a93
Fabrizio Furnari <ffurnari@wikimedia.org>
2024-10-25 18:06:13 +0200

    haproxykafka: profile and hiera files
    Added profile for haproxykafka class.
    
    Hiera data is provided for generic instances and text and upload cache
    hosts. As we're reusing existing profile class, already enabled on
    cp4037, this will be the hosts used to test PCC.
    
    Hosts: cp4037.ulsfo.wmnet,cp4038.ulsfo.wmnet
    Bug: T374128
    Change-Id: I55fd8cb37ae7c13672b01026bdd9984c458884c7

--- modules/profile/manifests/cache/haproxykafka.pp
+++ modules/profile/manifests/cache/haproxykafka.pp
@@ -1,9 +1,42 @@
 # SPDX-License-Identifier: Apache-2.0
+# == Class: profile::cache::haproxykafka
+#
+# Deploy haproxykafka instance with configuration and relative
+# auxiliary files
+#
 class profile::cache::haproxykafka(
-    Wmflib::Ensure $ensure = lookup('profile::cache::haproxykafka::ensure', {'default_value' => absent})
+    Wmflib::Ensure $ensure                        = lookup('profile::cache::haproxykafka::ensure'),
+    String $kafka_cluster_name                    = lookup('profile::cache::haproxykafka::kafka_cluster_name'),
+    Integer $workers                              = lookup('profile::cache::haproxykafka::workers'),
+    Float $message_buffer                         = lookup('profile::cache::haproxykafka::message_buffer'),
+    String $sdid                                  = lookup('profile::cache::haproxykafka::sdid'),
+    Haproxykafka::Socket $socket                  = lookup('profile::cache::haproxykafka::socket'),
+    Haproxykafka::Logparser $logparser            = lookup('profile::cache::haproxykafka::logparser'),
+    Haproxykafka::Kafka $kafka                    = lookup('profile::cache::haproxykafka::kafka'),
+    Haproxykafka::Monitoring $monitoring          = lookup('profile::cache::haproxykafka::monitoring'),
+    Haproxykafka::Transformrules $transform_rules = lookup('profile::cache::haproxykafka::transform_rules'),
 ) {
 
+    $kafka_cluster_cfg = kafka_config($kafka_cluster_name)
+    $bootstrap_servers = {
+        'rdkafka' => {
+            'bootstrap.servers' => $kafka_cluster_cfg['brokers']['ssl_string']
+        }
+    }
+
+    $config = {
+        workers         => $workers,
+        message_buffer  => $message_buffer,
+        sdid            => $sdid,
+        socket          => $socket,
+        logparser       => $logparser,
+        kafka           => deep_merge($kafka, $bootstrap_servers),
+        monitoring      => $monitoring,
+        transform_rules => $transform_rules,
+    }
+
     class { 'haproxykafka':
-        ensure => $ensure
+        ensure => $ensure,
+        config => $config,
     }
 }
