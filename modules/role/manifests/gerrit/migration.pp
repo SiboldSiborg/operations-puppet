a98a32d8af374ad9cec4452051bf80f4d6fd9250
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-10-28 13:41:36 +0100

    Deprecate system::role for remaining SRE-Collab roles
    system::role is a bit of a hack. In 21fa33b455bfd956f45525b7bb3c22c77ff3787d
    support was added to move the functionality into profile::base::production
    and simply set the role description via a Hiera flag.
    
    This converts the remaining roles by SRE-Collab.
    
    Change-Id: I750c3d4087307a44bb1b0efdf73cf977604e53e6

--- modules/role/manifests/gerrit/migration.pp
+++ modules/role/manifests/gerrit/migration.pp
@@ -1,12 +1,7 @@
 # temp allow rsyncing gerrit data to new server
 class role::gerrit::migration {
-
-    system::role {'gerrit::migration':
-        description => 'temp role to allow migrating Gerrit data to a new server',
-    }
-
-    include ::profile::base::production
-    include ::profile::firewall
-    include ::profile::gerrit::migration_base
-    include ::profile::gerrit::migration
+    include profile::base::production
+    include profile::firewall
+    include profile::gerrit::migration_base
+    include profile::gerrit::migration
 }
--- modules/role/manifests/phabricator/migration.pp
+++ modules/role/manifests/phabricator/migration.pp
@@ -1,12 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # temp allow rsyncing phabricator data to new servers
 class role::phabricator::migration {
-
-    system::role {'phabricator::migration':
-        description => 'temp role to allow migrating Phabricator data to a new server',
-    }
-
-    include ::profile::base::production
-    include ::profile::firewall
-    include ::profile::phabricator::migration
+    include profile::base::production
+    include profile::firewall
+    include profile::phabricator::migration
 }
