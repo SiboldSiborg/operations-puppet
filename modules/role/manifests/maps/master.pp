6da50454b878835a643f911491defb4336e2e5b5
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-10-28 13:45:53 +0100

    Deprecate system::role for maps
    system::role is a bit of a hack. In 21fa33b455bfd956f45525b7bb3c22c77ff3787d
    support was added to move the functionality into profile::base::production
    and simply set the role description via a Hiera flag.
    
    Also remove the outdated/incorrect ownership annotation for the roles.
    
    Change-Id: Icac5f756766573682b44ffada0aaa3532b49967a

--- modules/role/manifests/maps/master.pp
+++ modules/role/manifests/maps/master.pp
@@ -1,18 +1,12 @@
 # Sets up a maps server master
 class role::maps::master {
-    include ::profile::base::production
-    include ::profile::rsyslog::udp_localhost_compat
-    include ::profile::firewall
-    include ::profile::lvs::realserver
+    include profile::base::production
+    include profile::rsyslog::udp_localhost_compat
+    include profile::firewall
+    include profile::lvs::realserver
 
-    include ::profile::maps::apps
-    include ::profile::maps::osm_master
-    include ::profile::maps::tlsproxy
-    include ::profile::prometheus::postgres_exporter
-
-    system::role { 'maps::master':
-      ensure      => 'present',
-      description => 'Maps master (postgresql, kartotherian)',
-    }
-
+    include profile::maps::apps
+    include profile::maps::osm_master
+    include profile::maps::tlsproxy
+    include profile::prometheus::postgres_exporter
 }
--- modules/role/manifests/maps/replica.pp
+++ modules/role/manifests/maps/replica.pp
@@ -1,18 +1,12 @@
 # Sets up a maps server replica
 class role::maps::replica {
-    include ::profile::base::production
-    include ::profile::rsyslog::udp_localhost_compat
-    include ::profile::firewall
-    include ::profile::lvs::realserver
+    include profile::base::production
+    include profile::rsyslog::udp_localhost_compat
+    include profile::firewall
+    include profile::lvs::realserver
 
-    include ::profile::maps::apps
-    include ::profile::maps::osm_replica
-    include ::profile::maps::tlsproxy
-    include ::profile::prometheus::postgres_exporter
-
-    system::role { 'maps::replica':
-      ensure      => 'present',
-      description => 'Maps replica (postgresql, kartotherian)',
-    }
-
+    include profile::maps::apps
+    include profile::maps::osm_replica
+    include profile::maps::tlsproxy
+    include profile::prometheus::postgres_exporter
 }
