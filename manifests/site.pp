18a54c26bf3a16db77d986de22796c26643afb5b
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-11-06 14:53:52 +0100

    Add ganeti1045/ganeti1046 as Ganeti nodes
    Bug: T378921
    Change-Id: Ie1ebd11d6ed8494f7b40ba83874f797fea4ad486

--- manifests/site.pp
+++ manifests/site.pp
@@ -1327,11 +1327,11 @@ node /^lists2001\.wikimedia\./ {
     role(lists)
 }
 
-node /^ganeti10(09|1[0-9]|2[0-9]|3[0-9]|4[0-4])\.eqiad\./ {
+node /^ganeti10(09|1[0-9]|2[0-9]|3[0-9]|4[0-6])\.eqiad\./ {
     role(ganeti)
 }
 
-node /^ganeti10(4[5-9]|5[0-2]|)\.eqiad\./ {
+node /^ganeti10(4[7-9]|5[0-2]|)\.eqiad\./ {
     role(insetup::infrastructure_foundations)
 }
 
