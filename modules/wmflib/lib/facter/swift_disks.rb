bb2c906f8603ad0ade1a8ce485da1a8838ec7eff
Matthew Vernon <mvernon@wikimedia.org>
2024-11-06 11:48:03 +0000

    facts: adjust swift_disks fact to handle new SM kit
    The new SM Config-J hosts have duplicate by-path entries for the SSD partitions:
    
    ```
    root@ms-be2083:~# ls /dev/disk/by-path/*-part4
    /dev/disk/by-path/pci-0000:00:11.5-ata-1.0-part4
    /dev/disk/by-path/pci-0000:00:11.5-ata-2.0-part4
    /dev/disk/by-path/pci-0000:00:11.5-ata-1-part4
    /dev/disk/by-path/pci-0000:00:11.5-ata-2-part4
    ```
    
    Use a regex to match the two different types (ata on SM, scsi on Dell),
    and then adjust the ssds to take a slightly shorter substring, so that
    objects still excludes the SSDs.
    
    Tested by hand on an old Dell system (ms-be1082) and a new SM system
    (ms-be2083).
    
    Hosts: ms-be1082.eqiad.wmnet
    Bug: T371400
    Change-Id: I6cb12d61cf62c6a807ba0e326102c14bf9ce4d34

--- modules/wmflib/lib/facter/swift_disks.rb
+++ modules/wmflib/lib/facter/swift_disks.rb
@@ -5,9 +5,9 @@ Facter.add('swift_disks') do
   setcode do
     swift_disks = { }
     devices = Dir["/dev/disk/by-path/*"].map {|f| f.split('/')[-1]  }
-    swift_disks[:accounts] = devices.select {|d| d.end_with?('-part4') }.sort
-    swift_disks[:container] = devices.select {|d| d.end_with?('-part5') }.sort
-    ssds = swift_disks[:accounts].map { |d| d[0...-6] }
+    swift_disks[:accounts] = devices.select {|d| d.scan(/(ata-\d+\.\d+|scsi-\d+:\d+:\d+:\d+)-part4/).any? }.sort
+    swift_disks[:container] = devices.select {|d| d.scan(/(ata-\d+\.\d+|scsi-\d+:\d+:\d+:\d+)-part5/).any? }.sort
+    ssds = swift_disks[:accounts].map { |d| d[0...-8] }
     # disks may not be paritioned so we just get the route disk for this
     # This may be confusing need to check
     swift_disks[:objects] = devices.reject { |d| d.start_with?(*ssds) || d =~ (/-part[1-9]$/) }.sort
