078f586c6703041fdd54220145b39e754e130aa9
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-11-07 09:35:46 +0100

    kafka::certificate: Remove non-PKI code paths
    All nodes have been migrated to use PKI, so remove the option and the
    unused code path.
    
    Hosts: C:profile::cache::kafka::certificate
    Bug: T337825
    Change-Id: Ic6c070d76d777af94d8dffa46555e919210b4c35

--- modules/profile/manifests/cache/kafka/certificate.pp
+++ modules/profile/manifests/cache/kafka/certificate.pp
@@ -32,67 +32,21 @@ class profile::cache::kafka::certificate(
     String $ssl_cipher_suites          = lookup('profile::cache::kafka::certificate::ssl_cipher_suites', {'default_value' => 'ECDHE-ECDSA-AES256-GCM-SHA384'}),
     String $ssl_curves_list            = lookup('profile::cache::kafka::certificate::ssl_curves_list', {'default_value' => 'P-256'}),
     String $ssl_sigalgs_list           = lookup('profile::cache::kafka::certificate::ssl_sigalgs_list', {'default_value' => 'ECDSA+SHA256'}),
-    Boolean $use_pki_settings          = lookup('profile::cache::kafka::certificate::use_pki_settings', {'default_value' => false}),
 ){
     # TLS/SSL configuration
     $ssl_location = '/etc/varnishkafka/ssl'
 
-    if $use_pki_settings {
-        $ssl_files = profile::pki::get_cert('kafka', $certificate_name, {
-            'outdir'  => $ssl_location,
-            'owner'   => 'root',
-            'group'   => 'root',
-            'profile' => 'kafka_11',
-            notify    => Service['varnishkafka-all'],
-            }
-        )
+    $ssl_files = profile::pki::get_cert('kafka', $certificate_name, {
+        'outdir'  => $ssl_location,
+        'owner'   => 'root',
+        'group'   => 'root',
+        'profile' => 'kafka_11',
+        notify    => Service['varnishkafka-all'],
+    })
 
-        $ssl_key_location = $ssl_files['key']
-        $ssl_certificate_location = $ssl_files['chained']
-        $ssl_key_password = $ssl_key_pass
-
-    } else {
-        $ssl_location_private = '/etc/varnishkafka/ssl/private'
-
-        $ssl_key_password = $ssl_key_pass
-        $ssl_key_location_secrets_path = "certificates/${certificate_name}/${certificate_name}.key.private.pem"
-        $ssl_key_location = "${ssl_location_private}/${certificate_name}.key.pem"
-
-        $ssl_certificate_secrets_path = "certificates/${certificate_name}/${certificate_name}.crt.pem"
-        $ssl_certificate_location = "${ssl_location}/${certificate_name}.crt.pem"
-
-        $ssl_keystore_password = undef
-
-        file { $ssl_location:
-            ensure => 'directory',
-            owner  => 'root',
-            group  => 'root',
-            mode   => '0555',
-        }
-
-        file { $ssl_location_private:
-            ensure  => 'directory',
-            owner   => 'root',
-            group   => 'root',
-            mode    => '0500',
-            require => File[$ssl_location],
-        }
-
-        file { $ssl_key_location:
-            content => secret($ssl_key_location_secrets_path),
-            owner   => 'root',
-            group   => 'root',
-            mode    => '0400',
-            require => File[$ssl_location_private],
-        }
-
-        file { $ssl_certificate_location:
-            content => secret($ssl_certificate_secrets_path),
-            owner   => 'root',
-            group   => 'root',
-            mode    => '0444',
-        }
-    }
+    $ssl_key_location = $ssl_files['key']
+    $ssl_certificate_location = $ssl_files['chained']
+    $ssl_key_password = $ssl_key_pass
 
     if $use_internal_ca {
         $ssl_ca_location = profile::base::certificates::get_trusted_ca_path()
