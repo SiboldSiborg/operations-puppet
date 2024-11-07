659d0a3cc0e669531665c3588c06202d3dfcf73f
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-11-07 15:06:48 +0100

    cache::kafka::certificate: Remove $use_internal_ca
    When merging 078f586c6703041fdd54220145b39e754e130aa9 I realised that
    profile::cache::kafka::certificate::use_internal_ca isn't used anywhere
    any more and operates solely on the internal default.
    
    As such, remove the old code path which was only needed for cergen-based
    certs entirely.
    
    Hosts: C:profile::cache::kafka::certificate
    Bug: T337825
    Change-Id: Ibb431b269e327dbf5a26083d5d57d32a29af2c72

--- modules/profile/manifests/cache/kafka/certificate.pp
+++ modules/profile/manifests/cache/kafka/certificate.pp
@@ -18,17 +18,9 @@
 #   To find the certificate file secret() puppet paths.  You might want to
 #   change this if you are testing in Cloud VPS.  Default: varnishkafka.
 #
-# [*use_internal_ca*]
-#   If true, the CA cert.pem file will be assumed to be already installed at
-#   /etc/ssl/certs/wmf-ca-certificates.crt, and will be used as the ssl.ca.location
-#   for varnishkafka/librdkafka.  Default: true.  Set this to false if the
-#   certificate name you set is not signed by the Puppet CA, and the
-#   cergen created ca.crt.pem file will be used.
-#
 class profile::cache::kafka::certificate(
     Optional[String] $ssl_key_pass = lookup('profile::cache::kafka::certificate::ssl_key_password', {'default_value' => undef}),
     String $certificate_name           = lookup('profile::cache::kafka::certificate::certificate_name', {'default_value' => 'varnishkafka'}),
-    Boolean $use_internal_ca           = lookup('profile::cache::kafka::certificate::use_internal_ca', {'default_value' => true}),
     String $ssl_cipher_suites          = lookup('profile::cache::kafka::certificate::ssl_cipher_suites', {'default_value' => 'ECDHE-ECDSA-AES256-GCM-SHA384'}),
     String $ssl_curves_list            = lookup('profile::cache::kafka::certificate::ssl_curves_list', {'default_value' => 'P-256'}),
     String $ssl_sigalgs_list           = lookup('profile::cache::kafka::certificate::ssl_sigalgs_list', {'default_value' => 'ECDSA+SHA256'}),
@@ -47,19 +39,5 @@ class profile::cache::kafka::certificate(
     $ssl_key_location = $ssl_files['key']
     $ssl_certificate_location = $ssl_files['chained']
     $ssl_key_password = $ssl_key_pass
-
-    if $use_internal_ca {
-        $ssl_ca_location = profile::base::certificates::get_trusted_ca_path()
-    }
-    else {
-        $ssl_ca_location_secrets_path = "certificates/${certificate_name}/ca.crt.pem"
-        $ssl_ca_location = "${ssl_location}/ca.crt.pem"
-
-        file { $ssl_ca_location:
-            content => secret($ssl_ca_location_secrets_path),
-            owner   => 'root',
-            group   => 'root',
-            mode    => '0444',
-        }
-    }
+    $ssl_ca_location = profile::base::certificates::get_trusted_ca_path()
 }
