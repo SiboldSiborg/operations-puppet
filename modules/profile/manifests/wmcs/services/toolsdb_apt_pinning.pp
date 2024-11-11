fdaeb360cab769026295a857a48148b89233fff6
Francesco Negri <fnegri@wikimedia.org>
2024-11-11 16:06:12 +0100

    toolsdb: apply pinning to all debian versions
    No reason to check which version we are running. I am currently in the
    process of upgrading to Bookworm and 10.6 and I want the same pin on all
    toolsdb hosts.
    
    Bug: T352206
    Change-Id: I40790da1a746aaffc1d09425653809698a104420

--- modules/profile/manifests/wmcs/services/toolsdb_apt_pinning.pp
+++ modules/profile/manifests/wmcs/services/toolsdb_apt_pinning.pp
@@ -7,21 +7,17 @@ class profile::wmcs::services::toolsdb_apt_pinning (
         priority => -1,
     }
 
-    if debian::codename::eq('bullseye') {
-        # A pin with priority between 0 and 100 will allow the package to be installed
-        # but will not allow upgrading it (unless manually asked to do so).
-        apt::pin { 'toolsdb_mariadb_104_no_auto_updates':
-            package  => 'wmf-mariadb104',
-            pin      => 'version *',
-            priority => 90,
-        }
+    # A pin with priority between 0 and 100 will allow the package to be installed
+    # but will not allow upgrading it (unless manually asked to do so).
+    apt::pin { 'toolsdb_mariadb_104_no_auto_updates':
+      package  => 'wmf-mariadb104',
+      pin      => 'version *',
+      priority => 90,
+    }
 
-        apt::pin { 'toolsdb_no_mariadb_106':
-            package  => 'wmf-mariadb106',
-            pin      => 'version *',
-            priority => -1,
-        }
-    } else {
-        fail("${debian::codename()}: not supported by profile::wmcs::services::toolsdb_apt_pinning")
+    apt::pin { 'toolsdb_no_mariadb_106':
+      package  => 'wmf-mariadb106',
+      pin      => 'version *',
+      priority => -1,
     }
 }
