d64e2a20b5fc6acf7c76a343e522365ae2c3aff3
Ben Tullis <btullis@wikimedia.org>
2024-11-13 10:40:25 +0000

    Remove base::cloud_production profile from an-redacteddb1001
    This server should not extend profile::base::cloud_production profile,
    since is it more correctly a production host that is managed by the Data
    Platform Engineering group.
    
    Hosts: an-redacteddb1001.eqiad.wmnet
    Bug: T379571
    Change-Id: I15e148afc005b08c7237ecf7477aef38c8bca1ac

--- modules/role/manifests/wmcs/db/wikireplicas/dedicated/analytics_multiinstance.pp
+++ modules/role/manifests/wmcs/db/wikireplicas/dedicated/analytics_multiinstance.pp
@@ -6,7 +6,6 @@ class role::wmcs::db::wikireplicas::dedicated::analytics_multiinstance {
     include profile::base::production
     include profile::wmcs::db::wikireplicas::mariadb_multiinstance
     include profile::firewall
-    include profile::base::cloud_production
     include profile::wmcs::db::wikireplicas::views
     include profile::mariadb::check_private_data
     include profile::wmcs::db::wikireplicas::dedicated::analytics
