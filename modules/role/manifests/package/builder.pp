33a6a12acd7dfa7af74a051521232dae08f4530f
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-11-15 14:42:24 +0100

    Remove obsolete package::builder role
    The current role is just called builder.
    
    Hosts: C:role::package::builder
    Change-Id: I8d7db114fe17c64a564fa9864683bb4da1994c44

--- modules/role/manifests/package/builder.pp
+++
@@ -1,9 +0,0 @@
-# == Class: role::package::builder
-#
-# Role for package_builder
-#
-class role::package::builder {
-    include profile::base::production
-    include profile::firewall
-    include profile::package_builder
-}
