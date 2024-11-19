12326db49b505380c7e48bb6ca1190486a3b79a8
Keith Herron <kherron@wikimedia.org>
2024-11-19 16:27:22 -0500

    site: add aux-k8s codfw insetup roles
    add insetup entries for aux-k8s codfw VM install/prep
    
    Bug: T378986
    Bug: T378987
    Bug: T378988
    Change-Id: I50c8473e539efcaab6ef3798b45f1f105dd4da1c

--- manifests/site.pp
+++ manifests/site.pp
@@ -232,14 +232,26 @@ node /^aux-k8s-etcd100[3-5]\.eqiad\./ {
     role(etcd::v3::aux_k8s_etcd)
 }
 
+node /^aux-k8s-etcd100[3-5]\.codfw\./ {
+    role(insetup::observability)
+}
+
 # control-plane servers for aux kubernetes cluster
 node /^aux-k8s-ctrl100[23]\.eqiad\./ {
     role(aux_k8s::master)
 }
 
+node /^aux-k8s-ctrl100[23]\.codfw\./ {
+    role(insetup::observability)
+}
+
 # worker nodes for aux kubernetes cluster
 node /^aux-k8s-worker100[2-5]\.eqiad\./ {
     role(aux_k8s::worker)
+}
+
+node /^aux-k8s-worker100[2-5]\.codfw\./ {
+    role(insetup::observability)
 }
 
 # Primary bacula director and storage daemon
