37e096b5e7127bb25008fc299865771359211dba
Taavi Väänänen <hi@taavi.wtf>
2024-11-22 18:57:46 +0200

    O:openstack: Drop codfw1dev db role
    This was used by clouddb2002-dev which is being decom'd.
    
    Bug: T369308
    Change-Id: I19fcf44ce9fb3265f88865cf9082632332eb76f2

--- modules/role/manifests/wmcs/openstack/codfw1dev/db.pp
+++
@@ -1,7 +0,0 @@
-class role::wmcs::openstack::codfw1dev::db {
-    include profile::base::production
-    include profile::firewall
-    include profile::base::cloud_production
-
-    include profile::openstack::codfw1dev::db
-}
