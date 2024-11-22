20e3127284966acb40205ceba74a1063276d5d5c
Vgutierrez <vgutierrez@wikimedia.org>
2024-11-22 11:06:51 +0000

    Revert^2 "haproxykafka: working on TLS client authentication to kafka"
    This reverts commit 5a5516b137abe59534b1d3a8ac648369ec1808ba.
    and also brings back d8216067ecf2e0a31cad954a1350a4a7c389369e.
    
    This commit also ships ensure => absent spec tests and ensures
    that profile::pki::get_cert() doesn't get called if ensure => absent
    
    Reason for revert: "We need mTLS now"
    
    Change-Id: I2a72b0c5efe4cc6ec970b8c11fcd84eb34377b13

--- modules/haproxykafka/spec/classes/haproxykafka_spec.rb
+++ modules/haproxykafka/spec/classes/haproxykafka_spec.rb
@@ -5,64 +5,70 @@ describe 'haproxykafka' do
   on_supported_os(WMFConfig.test_on).each do |os, facts|
     context "On #{os}" do
       let(:facts) { facts }
-      context "On ensure present" do
-        let(:params) {
-          {
-            ensure: 'present',
-            user: 'haproxykafka',
-            config: {
-              workers: 1,
-              message_buffer: 1024.0,
-              sdid: 'haproxykafka@0',
-              socket: {
-                path: '/var/run/haproxykafka.sock',
-                mode: '0622',
-                user: 'haproxykafka',
-                group: 'haproxykafka',
-                batch_size: 1024,
-                batch_deadline: '100ms',
-              },
-              logparser: {
-                batch_size: 1024,
-                batch_deadline: '100ms',
-              },
-              kafka: {
-                topic: 'test-topic',
-                dlq_topic: 'test-dlq',
-                flush_timeout: 100,
-                batch_size: 1024,
-                batch_deadline: '100ms',
-                rdkafka: {
-                  acks: "all",
-                  'client.id': "test-hostname",
-                  'security.protocol': "SSL",
-                  'ssl.ca.location': "/etc/ssl/certs/wmf-ca-certificates.crt",
-                  'ssl.cipher.suites': "ECDHE-ECDSA-AES256-GCM-SHA384",
-                  'ssl.curves.list': "P-256",
-                  'ssl.sigalgs.list': "ECDSA+SHA256",
-                  'queue.buffering.max.messages': 720_000,
-                  'queue.buffering.max.ms': 1000,
-                  'batch.num.messages': 9000,
-                  'compression.codec': "snappy",
-                  'topic.request.required.acks': 1,
-                },
-              },
-              monitoring: {
-                enable_pprof: true,
-                enable_prometheus: true,
-                server_bind: ":1234",
-                prometheus_prefix: "haproxykafka_",
-                prometheus_parsing_buckets: [1.0e-5, 1.0e-4],
-                prometheus_processing_buckets: [1.0e-5, 1.0e-4],
-              },
-              transform_rules: {
-                haproxy_format: '02/Jan/2006:15:04:05.000',
-                date_format: "2006-01-02T15:04:05Z",
-                date_tz: "UTC",
+      let(:params) {
+        {
+          user: 'haproxykafka',
+          config: {
+            workers: 1,
+            message_buffer: 1024.0,
+            sdid: 'haproxykafka@0',
+            socket: {
+              path: '/var/run/haproxykafka.sock',
+              mode: '0622',
+              user: 'haproxykafka',
+              group: 'haproxykafka',
+              batch_size: 1024,
+              batch_deadline: '100ms',
+            },
+            logparser: {
+              batch_size: 1024,
+              batch_deadline: '100ms',
+            },
+            kafka: {
+              topic: 'test-topic',
+              dlq_topic: 'test-dlq',
+              flush_timeout: 100,
+              batch_size: 1024,
+              batch_deadline: '100ms',
+              rdkafka: {
+                acks: "all",
+                'client.id': "test-hostname",
+                'security.protocol': "SSL",
+                'ssl.ca.location': "/etc/ssl/certs/wmf-ca-certificates.crt",
+                'ssl.certificate.location': "/tmp/test.file",
+                'ssl.key.location': "/tmp/test.file",
+                'ssl.cipher.suites': "ECDHE-ECDSA-AES256-GCM-SHA384",
+                'ssl.curves.list': "P-256",
+                'ssl.sigalgs.list': "ECDSA+SHA256",
+                'queue.buffering.max.messages': 720_000,
+                'queue.buffering.max.ms': 1000,
+                'batch.num.messages': 9000,
+                'compression.codec': "snappy",
+                'topic.request.required.acks': 1,
               },
             },
-          }
+            monitoring: {
+              enable_pprof: true,
+              enable_prometheus: true,
+              server_bind: ":1234",
+              prometheus_prefix: "haproxykafka_",
+              prometheus_parsing_buckets: [1.0e-5, 1.0e-4],
+              prometheus_processing_buckets: [1.0e-5, 1.0e-4],
+            },
+            transform_rules: {
+              haproxy_format: '02/Jan/2006:15:04:05.000',
+              date_format: "2006-01-02T15:04:05Z",
+              date_tz: "UTC",
+            },
+          },
         }
+      }
+      context "On ensure absent" do
+        let(:params) { super().merge('ensure' => 'absent') }
+        it { is_expected.to compile.with_all_deps }
+      end
+      context "On ensure present" do
+        let(:params) { super().merge('ensure' => 'present') }
         it { is_expected.to compile.with_all_deps }
         it { is_expected.to contain_package('haproxykafka') }
         it { is_expected.to contain_file('/var/run/haproxykafka').with({
@@ -106,6 +112,8 @@ kafka:
     client.id: test-hostname
     security.protocol: SSL
     ssl.ca.location: "/etc/ssl/certs/wmf-ca-certificates.crt"
+    ssl.certificate.location: "/tmp/test.file"
+    ssl.key.location: "/tmp/test.file"
     ssl.cipher.suites: ECDHE-ECDSA-AES256-GCM-SHA384
     ssl.curves.list: P-256
     ssl.sigalgs.list: ECDSA+SHA256
