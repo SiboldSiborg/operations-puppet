843fa5ef813112761be72a885eddd394a778d00b
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-11-25 10:53:53 +0100

    Remove ganeti role from ganeti7003/7004
    Change-Id: Ifd9fe1a4141cebb4c314c1e9e82896d6e5592381

--- manifests/site.pp
+++ manifests/site.pp
@@ -1400,9 +1400,15 @@ node /^ganeti600[1234]\.drmrs\./ {
     role(ganeti)
 }
 
-node /^ganeti700[1234]\.magru\./ {
+node /^ganeti700[12]\.magru\./ {
     role(ganeti)
 }
+
+# magru rework
+node /^ganeti700[34]\.magru\./ {
+    role(insetup::infrastructure_foundations)
+}
+
 
 # gitlab servers - eqiad (T274459, T301177)
 node /^gitlab1003\.wikimedia\./ {
