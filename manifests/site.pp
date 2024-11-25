b9a0aff6c5e83876381f870d150a9e2767d46379
David Caro <dcaro@wikimedia.org>
2024-11-25 15:14:38 +0100

    cloudcephmon1004: provision as mon
    This should be the last of the mons, after this we can start
    decomissioning the old ones.
    
    Bug: T364870
    Signed-off-by: David Caro <dcaro@wikimedia.org>
    Change-Id: I9f717fe7a3bd527e501e9372256443798caa5b7b

--- manifests/site.pp
+++ manifests/site.pp
@@ -1776,13 +1776,8 @@ node /^cephosd100[12345]\.eqiad\./ {
 }
 
 # cloudceph monitor nodes
-node /^cloudcephmon100[1-3,5-6]\.eqiad\./ {
+node /^cloudcephmon100[1-6]\.eqiad\./ {
     role(wmcs::ceph::mon)
-}
-
-# new cloudceph  nodes
-node /^cloudcephmon100[4-6]\.eqiad\./ {
-    role(insetup::wmcs)
 }
 
 # cloudceph storage nodes
