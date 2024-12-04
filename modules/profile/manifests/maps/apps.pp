34dab3cd63ff32401b68132e2a624287d60852a1
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-12-04 14:24:23 +0100

    maps: Allow disabling the installation of kartotherian
    The new Bookworm maps nodes won't run kartotherian anymore (which will
    run on wikikube instead); add a profile option to prevent installing
    it.
    
    Also remove the obsolete osm_master Hiera check (was missed in
    previous cleanup)
    
    Hosts: maps1008.eqiad.wmnet, maps1009.eqiad.wmnet
    Bug: T381565
    Change-Id: I92acf05a9709c368757165e30a893e66f8ad2150

--- modules/profile/manifests/maps/apps.pp
+++ modules/profile/manifests/maps/apps.pp
@@ -1,21 +1,22 @@
 # SPDX-License-Identifier: Apache-2.0
 class profile::maps::apps(
-    String $osm_engine = lookup('profile::maps::osm_master::engine', { 'default_value' => 'osm2pgsql' }),
     String $pgsql_kartotherian_pass = lookup('profile::maps::osm_master::kartotherian_pass'),
     String $kartotherian_storage_id = lookup('profile::maps::apps::kartotherian_storage_id'),
     String $wikidata_query_service = lookup('profile::maps::apps::wikidata_query_service'),
+    Boolean $disable_kartotherian = lookup('profile::maps::disable_kartotherian', { 'default_value' => false }),
 ) {
+    unless $disable_kartotherian {
+        $osm_dir = '/srv/osm'
+        $contact_groups = 'admins,team-interactive'
 
-    $osm_dir = '/srv/osm'
-    $contact_groups = 'admins,team-interactive'
+        class { 'kartotherian':
+            pgsql_pass             => $pgsql_kartotherian_pass,
+            contact_groups         => $contact_groups,
+            storage_id             => $kartotherian_storage_id,
+            wikidata_query_service => $wikidata_query_service,
+        }
 
-    class { 'kartotherian':
-        pgsql_pass             => $pgsql_kartotherian_pass,
-        contact_groups         => $contact_groups,
-        storage_id             => $kartotherian_storage_id,
-        wikidata_query_service => $wikidata_query_service,
+        # those fonts are needed for the new maps style (brighmed)
+        ensure_packages(['fonts-noto', 'fonts-noto-cjk', 'fonts-noto-unhinted'])
     }
-
-    # those fonts are needed for the new maps style (brighmed)
-    ensure_packages(['fonts-noto', 'fonts-noto-cjk', 'fonts-noto-unhinted'])
 }
