e84a7ba85e3b1f2451525e2cc0bdb58b4abdbbe4
Dreamy Jazz <wpgbrown@wikimedia.org>
2024-12-04 11:49:44 +0000

    Update MediaModeration module to run scans automatically
    Why:
    * The Trust and Safety Product Team want to run MediaModeration
      scans on all WMF wikis automatically over newly uploaded images,
      now that the backlog of unscanned images has been processed.
    * This involves running the scanning script on all wikis hourly,
      except Wikimedia Commons where the scan can be run continuously
      because the upload rate is fast enough.
    
    What:
    * Add to mediamoderation.pp the following:
    ** A periodic_job that runs the scanning script every hour for
       all wikis except Wikimedia Commons and wikis that are not yet
       fully created (in the preinstall dblist).
    ** A periodic_job queued every hour that runs the scanning script
       on Wikimedia Commons with a slow enough sleep that the script
       will always have another image to process. This script is set
       to be exited through the 'timeout' command so it runs for at
       most just under an hour.
    
    Depends-On: If0f75bca11b9a2048babec338f7b58366fc48223
    Bug: T355169
    Change-Id: I584843882451a8672599694bcab7189c84690916

--- modules/profile/manifests/mediawiki/maintenance/mediamoderation.pp
+++ modules/profile/manifests/mediawiki/maintenance/mediamoderation.pp
@@ -5,4 +5,16 @@ class profile::mediawiki::maintenance::mediamoderation {
         command  => '/usr/local/bin/foreachwikiindblist /srv/mediawiki/dblists/all.dblist extensions/MediaModeration/maintenance/updateMetrics.php --verbose',
         interval => '*-*-* 04:32:00',
     }
+
+    # Run a scan over newly uploaded images on all WMF wikis (except Wikimedia Commons) every hour (T355169)
+    profile::mediawiki::periodic_job { 'mediamoderation-hourlyScan':
+        command  => '/usr/local/bin/foreachwikiindblist "all.dblist - mediamoderation-continuous-scan.dblist - preinstall.dblist" extensions/MediaModeration/maintenance/scanFilesInScanTable.php --use-jobqueue --sleep=1 --poll-sleep=10 --last-checked=never --verbose',
+        interval => '*-*-* *:02:00',
+    }
+
+    # Run a continuous scan on Wikimedia Commons, restarted every hour (T355169)
+    profile::mediawiki::periodic_job { 'mediamoderation-continuousScan-commonswiki':
+        command  => 'timeout 3500 /usr/local/bin/mwscript extensions/MediaModeration/maintenance/scanFilesInScanTable.php --wiki=commonswiki --use-jobqueue --poll-sleep=30 --sleep=60 --last-checked=never --verbose',
+        interval => '*-*-* *:34:00',
+    }
 }
