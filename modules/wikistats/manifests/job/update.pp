d424049cba6657d0cf09831b62fbb5e3a100e821
Daniel Zahn <dzahn@wikimedia.org>
2024-12-13 14:53:32 -0800

    wikistats: add a second timer to pull extended info
    Add a second timer for each table that pulls extended info
    in addition to normal number fetching, 30 minutes after the
    normal update.
    
    Bug: T381623
    Change-Id: I7cb186bb9c7fab34f94786828d52eb276dc723ef

--- modules/wikistats/manifests/job/update.pp
+++ modules/wikistats/manifests/job/update.pp
@@ -7,20 +7,36 @@ define wikistats::job::update (
     Optional[String] $day = undef,
     Wmflib::Ensure $ensure = 'present',
 ){
+
+    $minute_extinfo=$minute+30;
+
     if $day {
       $interval = "${day} *-*-* ${hour}:${minute}:00"
+      $interval_extinfo = "${day} *-*-* ${hour}:${minute_extinfo}:00"
     } else {
       $interval = "*-*-* ${hour}:${minute}:00"
+      $interval_extinfo = "*-*-* ${hour}:${minute_extinfo}:00"
     }
+
     systemd::timer::job { "wikistats-update-${name}":
         ensure          => $ensure,
         user            => 'wikistatsuser',
         description     => "pull fresh numbers for table ${name}",
-        command         => "/usr/bin/php /usr/lib/wikistats/update.php ${project} extinfo",
+        command         => "/usr/bin/php /usr/lib/wikistats/update.php ${project}",
         logging_enabled => true,
         logfile_basedir => '/var/log/wikistats/',
         logfile_name    => "update-${name}.log",
         interval        => {'start' => 'OnCalendar', 'interval' => $interval},
     }
 
+    systemd::timer::job { "wikistats-extinfo-update-${name}":
+        ensure          => $ensure,
+        user            => 'wikistatsuser',
+        description     => "pull extended info for table ${name}",
+        command         => "/usr/bin/php /usr/lib/wikistats/update.php ${project} extinfo",
+        logging_enabled => true,
+        logfile_basedir => '/var/log/wikistats/',
+        logfile_name    => "update-extinfo-${name}.log",
+        interval        => {'start' => 'OnCalendar', 'interval' => $interval_extinfo},
+    }
 }
