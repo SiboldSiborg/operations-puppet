f247bed3c69784d629e6da1e9832a4a4173fadd3
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-12-18 13:40:43 +0100

    Deprecate system::role for Druid roles
    system::role is a bit of a hack. In 21fa33b455bfd956f45525b7bb3c22c77ff3787d
    support was added to move the functionality into profile::base::production
    and simply set the role description via a Hiera flag.
    
    Change-Id: I2a02bcabea164b1dc0cf3196f1791acb1c10c919

--- modules/role/manifests/druid/public/worker.pp
+++ modules/role/manifests/druid/public/worker.pp
@@ -2,25 +2,22 @@
 # Sets up the Druid public cluster for use with AQS and wikistats 2.0.
 #
 class role::druid::public::worker {
-    system::role { 'druid::public::worker':
-        description => "Druid worker in the public-${::site} cluster",
-    }
 
     class { '::lvs::realserver': }
 
-    include ::profile::base::production
-    include ::profile::firewall
-    include ::profile::java
-    include ::profile::druid::broker
-    include ::profile::druid::coordinator
-    include ::profile::druid::historical
-    include ::profile::druid::middlemanager
-    include ::profile::druid::overlord
-    include ::profile::prometheus::druid_exporter
-    include ::profile::druid::conftool
+    include profile::base::production
+    include profile::firewall
+    include profile::java
+    include profile::druid::broker
+    include profile::druid::coordinator
+    include profile::druid::historical
+    include profile::druid::middlemanager
+    include profile::druid::overlord
+    include profile::prometheus::druid_exporter
+    include profile::druid::conftool
 
-    include ::profile::kerberos::client
-    include ::profile::kerberos::keytabs
+    include profile::kerberos::client
+    include profile::kerberos::keytabs
 
     # Zookeeper is co-located on some public druid hosts, but not all.
     if $::fqdn in $::profile::druid::common::zookeeper_hosts {
